bc83afd3c19e25f03a8137df64891000e3adf9d4
borutorinred <37692980+borutorinred@users.noreply.github.com>
2018-11-16 16:59:38 +0600

    sesc-bubble-sort
    sesc-bubble-sort

---
+++ sesc-bubble-sort.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+
+  int *arr = (int*)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++)
+    scanf("%d", &arr[i]);
+
+  for (int i = n; i-->1;)
+    for (int j = 0; j < i; j++)
+      if (arr[j] < arr[j + 1])
+        arr[j] ^= arr[j + 1] ^= arr[j] ^= arr[j + 1];
+  
+  for (int i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+  
+  return 0;
+}
